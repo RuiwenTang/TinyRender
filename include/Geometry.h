9ebb2d0dad1a48d253d91d2c82920a3525442fb2
RuiwenTang <tangruiwen1989@gmail.com>
2023-04-15 16:13:53 +0800

    Move edge and polygon to common header file

--- include/Geometry.h
+++ include/Geometry.h
@@ -8,6 +8,10 @@
 #include <glm/glm.hpp>
 #include <iostream>
 #include <limits>
+#include <memory>
+#include <vector>
+
+#include "LinkedList.h"
 
 namespace TRM {
 
@@ -59,5 +63,226 @@ static inline glm::vec<DIM, T, glm::defaultp> Normalize(
 static inline Matrix IdentityMatrix() { return glm::identity<Matrix>(); }
 
 float degToRadian(float deg);
+
+struct Object {
+  virtual ~Object() = default;
+};
+
+struct Edge;
+
+struct Vertex : public Object {
+  // list
+  Vertex* prev = nullptr;
+  Vertex* next = nullptr;
+
+  /**
+   * All edge above and end with this vertex
+   *
+   *          head           tail
+   *              \   ...    /
+   *                   v
+   *
+   */
+  LinkedList<Edge> edge_above = {};
+  /**
+   *  All edge below this vertex
+   *
+   *
+   *              v
+   *             / \
+   *          head  tail
+   *          /      \
+   *            ...
+   *
+   */
+  LinkedList<Edge> edge_below = {};
+
+  // left enclosing edge during sweep line
+  Edge* left = nullptr;
+  // right enclosing edge during sweep line
+  Edge* right = nullptr;
+
+  glm::vec2 point = {};
+
+  Vertex() = default;
+  Vertex(glm::vec2 const& p) : point(p) {}
+
+  ~Vertex() override = default;
+
+  bool is_connected() const { return edge_above.head || edge_below.head; }
+
+  void insert_above(Edge* e);
+  void insert_below(Edge* e);
+};
+
+struct VertexCompare {
+  inline bool operator()(Vertex* v1, Vertex* v2) {
+    return Compare(v1->point, v2->point);
+  }
+
+  static bool Compare(glm::vec2 const& a, glm::vec2 const& b);
+};
+
+struct VertexList : public LinkedList<Vertex> {
+  VertexList() = default;
+  VertexList(Vertex* head, Vertex* tail) : LinkedList(head, tail) {}
+
+  void insert(Vertex* v, Vertex* prev, Vertex* next);
+  void remove(Vertex* v);
+
+  void append(VertexList const& other);
+  void append(Vertex* v);
+  void prepend(Vertex* v);
+
+  void close();
+};
+
+struct Polygon;
+
+struct Edge : public Object {
+  Vertex* top = nullptr;
+  Vertex* bottom = nullptr;
+
+  Edge* above_prev = nullptr;
+  Edge* above_next = nullptr;
+  Edge* below_prev = nullptr;
+  Edge* below_next = nullptr;
+
+  // left edge in active list during sweep line
+  Edge* left = nullptr;
+  // right edge in active list during sweep line
+  Edge* right = nullptr;
+
+  Edge* right_poly_prev = nullptr;
+  Edge* right_poly_next = nullptr;
+  Edge* left_poly_prev = nullptr;
+  Edge* left_poly_next = nullptr;
+
+  // left polygon during sweep line
+  Polygon* left_poly = nullptr;
+  // right polygon during sweep line
+  Polygon* right_poly = nullptr;
+
+  bool used_in_left = false;
+  bool used_in_right = false;
+
+  int32_t winding = 1;
+
+  Edge(Vertex* top, Vertex* bottom, int32_t winding);
+
+  ~Edge() override = default;
+
+  // https://stackoverflow.com/questions/1560492/how-to-tell-whether-a-point-is-to-the-right-or-left-side-of-a-line
+  // return > 0 means point in left
+  // return < 0 means point in right
+  double side_dist(glm::vec2 const& p);
+
+  bool is_right_of(glm::vec2 const& p) { return side_dist(p) < 0.0; }
+
+  bool is_left_of(glm::vec2 const& p) { return side_dist(p) > 0.0; }
+
+  // https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+  bool intersect(Edge* other, glm::vec2* point);
+
+  void recompute();
+
+  void set_bottom(Vertex* v);
+
+  void set_top(Vertex* v);
+
+ private:
+  double le_a;
+  double le_b;
+  double le_c;
+};
+
+enum class Side {
+  kLeft,
+  kRight,
+};
+
+class ObjectHeap;
+
+struct MonotonePolygon : public Object {
+  Side side;
+
+  Edge* first = nullptr;
+  Edge* last = nullptr;
+
+  int32_t winding;
+
+  MonotonePolygon* prev = nullptr;
+  MonotonePolygon* next = nullptr;
+
+  MonotonePolygon(Edge* edge, Side side, int32_t winding)
+      : side(side), winding(winding) {
+    add_edge(edge);
+  }
+
+  ~MonotonePolygon() override = default;
+
+  void add_edge(Edge* edge);
+};
+
+enum class FillType {
+  kWinding,
+  kEvenOdd,
+};
+
+struct Polygon : public Object {
+  Vertex* first_vert;
+  int32_t winding;
+
+  // vertex count
+  int32_t count = 0;
+  Polygon* parent = nullptr;
+  Polygon* next = nullptr;
+
+  MonotonePolygon* head = nullptr;
+  MonotonePolygon* tail = nullptr;
+
+  Polygon(Vertex* first, int32_t winding)
+      : first_vert(first), winding(winding) {}
+
+  ~Polygon() override = default;
+
+  Polygon* add_edge(Edge* e, Side side, ObjectHeap* heap);
+
+  Vertex* last_vertex() const;
+};
+
+struct ActiveEdgeList : public LinkedList<Edge> {
+  ActiveEdgeList() = default;
+  ~ActiveEdgeList() = default;
+
+  void insert(Edge* edge, Edge* prev, Edge* next);
+  void insert(Edge* edge, Edge* prev);
+
+  void append(Edge* edge);
+  void remove(Edge* edge);
+
+  bool contains(Edge* edge);
+
+  // move event point from current to dst
+  void rewind(Vertex** current, Vertex* dst);
+
+  void find_enclosing(Vertex* v, Edge** left, Edge** right);
+};
+
+class ObjectHeap {
+ public:
+  ObjectHeap() = default;
+  ~ObjectHeap() = default;
+
+  template <class T, class... Args>
+  T* Allocate(Args&&... args) {
+    objs_.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+
+    return static_cast<T*>(objs_.back().get());
+  }
+
+ private:
+  std::vector<std::unique_ptr<Object>> objs_ = {};
+};
 
 }  // namespace TRM
--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -12,4 +12,310 @@ Matrix perspective(float fovy, float aspect, float near, float far) {
 
 float degToRadian(float deg) { return deg * PI / 180.f; }
 
+void Vertex::insert_above(Edge* e) {
+  if (e->top->point == e->bottom->point ||                     // no edge
+      VertexCompare::Compare(e->bottom->point, e->top->point)  // not above
+  ) {
+    return;
+  }
+
+  Edge* above_prev = nullptr;
+  Edge* above_next = nullptr;
+
+  for (above_next = edge_above.head; above_next != nullptr;
+       above_next = above_next->above_next) {
+    if (above_next->is_right_of(e->top->point)) {
+      break;
+    }
+
+    above_prev = above_next;
+  }
+
+  LinkedList<Edge>::Insert<&Edge::above_prev, &Edge::above_next>(
+      e, above_prev, above_next, &edge_above.head, &edge_above.tail);
+}
+
+void Vertex::insert_below(Edge* e) {
+  if (e->top->point == e->bottom->point ||                     // no edge
+      VertexCompare::Compare(e->bottom->point, e->top->point)  // not below
+  ) {
+    return;
+  }
+
+  Edge* below_prev = nullptr;
+  Edge* below_next = nullptr;
+
+  for (below_next = edge_below.head; below_next != nullptr;
+       below_next = below_next->below_next) {
+    if (below_next->is_right_of(e->bottom->point)) {
+      break;
+    }
+
+    below_prev = below_next;
+  }
+
+  LinkedList<Edge>::Insert<&Edge::below_prev, &Edge::below_next>(
+      e, below_prev, below_next, &edge_below.head, &edge_below.tail);
+}
+
+bool VertexCompare::Compare(const glm::vec2& a, const glm::vec2& b) {
+  return a.y < b.y || (a.y == b.y && a.x < b.x);
+}
+
+void VertexList::insert(Vertex* v, Vertex* prev, Vertex* next) {
+  Insert<&Vertex::prev, &Vertex::next>(v, prev, next, &head, &tail);
+}
+
+void VertexList::remove(Vertex* v) {
+  Remove<&Vertex::prev, &Vertex::next>(v, &head, &tail);
+}
+
+void VertexList::append(VertexList const& other) {
+  if (other.head == nullptr) {
+    return;
+  }
+
+  if (tail) {
+    tail->next = other.head;
+    other.head->prev = tail;
+  } else {
+    head = other.head;
+  }
+
+  tail = other.tail;
+}
+
+void VertexList::append(Vertex* v) { insert(v, tail, nullptr); }
+
+void VertexList::prepend(Vertex* v) { insert(v, nullptr, head); }
+
+void VertexList::close() {
+  if (head && tail) {
+    tail->next = head;
+    head->prev = tail;
+  }
+}
+
+Edge::Edge(Vertex* top, Vertex* bottom, int32_t winding)
+    : top(top),
+      bottom(bottom),
+      winding(winding),
+      le_a(static_cast<double>(bottom->point.y) - top->point.y),
+      le_b(static_cast<double>(top->point.x) - bottom->point.x),
+      le_c(static_cast<double>(top->point.y) * bottom->point.x -
+           static_cast<double>(top->point.x) * bottom->point.y) {}
+
+double Edge::side_dist(const glm::vec2& p) {
+  return le_a * p.x + le_b * p.y + le_c;
+}
+
+bool Edge::intersect(Edge* other, glm::vec2* point) {
+  if (top == other->top || bottom == other->bottom || top == other->bottom ||
+      bottom == other->top) {
+    return false;
+  }
+
+  // check if two aabb bounds is intersect
+  if (std::min(top->point.x, bottom->point.x) >
+          std::max(other->top->point.x, other->bottom->point.x) ||
+      std::max(top->point.x, bottom->point.x) <
+          std::min(other->top->point.x, other->bottom->point.x) ||
+      std::min(top->point.y, bottom->point.y) >
+          std::max(other->top->point.y, other->bottom->point.y) ||
+      std::max(top->point.y, bottom->point.y) <
+          std::min(other->top->point.y, other->bottom->point.y)) {
+    return false;
+  }
+
+  double denom = le_a * other->le_b - le_b * other->le_a;
+
+  if (denom == 0.0) {
+    return false;
+  }
+
+  double scale = 1.0 / denom;
+
+  point->x = glm::round(
+      static_cast<float>((le_b * other->le_c - other->le_b * le_c) * scale));
+  point->y = glm::round(
+      static_cast<float>((other->le_a * le_c - le_a * other->le_c) * scale));
+
+  if (glm::isinf(point->x) || glm::isinf(point->y)) {
+    return false;
+  }
+
+  return true;
+}
+
+void Edge::recompute() {
+  le_a = static_cast<double>(bottom->point.y) - top->point.y;
+  le_b = static_cast<double>(top->point.x) - bottom->point.x;
+  le_c = static_cast<double>(top->point.y) * bottom->point.x -
+         static_cast<double>(top->point.x) * bottom->point.y;
+}
+
+void Edge::set_top(Vertex* v) {
+  // remove this edge from top's below list
+  LinkedList<Edge>::Remove<&Edge::below_prev, &Edge::below_next>(
+      this, &top->edge_below.head, &top->edge_below.tail);
+  // update top vertex
+  top = v;
+  // recompute line equation
+  recompute();
+  // insert self to new top's below list
+  top->insert_below(this);
+}
+
+void Edge::set_bottom(Vertex* v) {
+  // remove this edge from bottom's above list
+  LinkedList<Edge>::Remove<&Edge::above_prev, &Edge::above_next>(
+      this, &bottom->edge_above.head, &bottom->edge_above.tail);
+  // update bottom vertex
+  bottom = v;
+  // recompute line equation
+  recompute();
+  // insert self to new bottom's above list
+  bottom->insert_above(this);
+}
+
+void ActiveEdgeList::insert(Edge* edge, Edge* prev, Edge* next) {
+  LinkedList<Edge>::Insert<&Edge::left, &Edge::right>(edge, prev, next, &head,
+                                                      &tail);
+}
+
+void ActiveEdgeList::insert(Edge* edge, Edge* prev) {
+  auto next = prev ? prev->right : head;
+
+  insert(edge, prev, next);
+}
+
+void ActiveEdgeList::append(Edge* edge) { insert(edge, tail, nullptr); }
+
+void ActiveEdgeList::remove(Edge* edge) {
+  LinkedList<Edge>::Remove<&Edge::left, &Edge::right>(edge, &head, &tail);
+}
+
+bool ActiveEdgeList::contains(Edge* edge) {
+  return edge->left || edge->right || head == edge;
+}
+
+void ActiveEdgeList::rewind(Vertex** current, Vertex* dst) {
+  if (!current || *current == dst ||
+      VertexCompare::Compare((*current)->point, dst->point)) {
+    return;
+  }
+
+  Vertex* v = *current;
+
+  while (v != dst) {
+    v = v->prev;
+
+    for (auto e = v->edge_below.head; e; e = e->below_next) {
+      this->remove(e);
+    }
+
+    auto left = v->left;
+
+    for (auto e = v->edge_above.head; e; e = e->above_next) {
+      this->insert(e, left);
+      left = e;
+
+      auto top = e->top;
+      if (VertexCompare::Compare(top->point, dst->point) &&
+          ((top->left && !top->left->is_left_of(e->top->point)) ||
+           (top->right && !top->right->is_right_of(e->top->point)))) {
+        dst = top;
+      }
+    }
+  }
+
+  *current = v;
+}
+
+void ActiveEdgeList::find_enclosing(Vertex* v, Edge** left, Edge** right) {
+  if (v->edge_above.head && v->edge_above.tail) {
+    *left = v->edge_above.head->left;
+    *right = v->edge_above.tail->right;
+    return;
+  }
+
+  Edge* prev = nullptr;
+  Edge* next = nullptr;
+
+  // walk through aet to get left most edge
+  for (prev = tail; prev != nullptr; prev = prev->left) {
+    if (prev->is_left_of(v->point)) {
+      break;
+    }
+    next = prev;
+  }
+
+  *left = prev;
+  *right = next;
+}
+
+void MonotonePolygon::add_edge(Edge* edge) {
+  if (side == Side::kRight) {
+    LinkedList<Edge>::Insert<&Edge::right_poly_prev, &Edge::right_poly_next>(
+        edge, last, nullptr, &first, &last);
+  } else {
+    LinkedList<Edge>::Insert<&Edge::left_poly_prev, &Edge::left_poly_next>(
+        edge, last, nullptr, &first, &last);
+  }
+}
+
+Polygon* Polygon::add_edge(Edge* e, Side side, ObjectHeap* heap) {
+  auto p_parent = this->parent;
+  auto poly = this;
+
+  if (side == Side::kRight) {
+    if (e->used_in_right) {
+      return this;
+    }
+  } else {
+    if (e->used_in_left) {
+      return this;
+    }
+  }
+
+  if (p_parent) {
+    this->parent = p_parent = nullptr;
+  }
+
+  if (!this->tail) {
+    this->head = this->tail = heap->Allocate<MonotonePolygon>(e, side, winding);
+    this->count += 2;
+  } else if (e->bottom == this->tail->last->bottom) {
+    // close this polygon
+    return poly;
+  } else if (side == this->tail->side) {
+    this->tail->add_edge(e);
+    this->count++;
+  } else {
+    e = heap->Allocate<Edge>(this->tail->last->bottom, e->bottom, 1);
+    this->tail->add_edge(e);
+    this->count++;
+
+    if (p_parent) {
+      p_parent->add_edge(e, side, heap);
+      poly = p_parent;
+    } else {
+      auto m = heap->Allocate<MonotonePolygon>(e, side, this->winding);
+      m->prev = this->tail;
+      this->tail->next = m;
+      this->tail = m;
+    }
+  }
+
+  return poly;
+}
+
+Vertex* Polygon::last_vertex() const {
+  if (tail) {
+    return tail->last->bottom;
+  }
+
+  return first_vert;
+}
+
 }  // namespace TRM
